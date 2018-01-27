7fcb6e402e25409be0faff85da9b1ea1ad94c9f5
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2018-01-27 04:54:31 +0300

    pixel reading performance improvements

--- android/src/main/java/com/mol42/imagedata/RNReactNativeImageDataModule.java
+++ android/src/main/java/com/mol42/imagedata/RNReactNativeImageDataModule.java
@@ -10,6 +10,7 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -33,44 +34,50 @@ public class RNReactNativeImageDataModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getPixels(String filePath, final Promise promise) {
+  public void getPixels(String filePath) {
     try {
-      WritableNativeMap result = new WritableNativeMap();
-      WritableNativeArray pixels = new WritableNativeArray();
-
+      
       Bitmap bitmap = BitmapFactory.decodeFile(filePath);
       if (bitmap == null) {
-        promise.reject("Failed to decode. Path is incorrect or image is corrupted");
+        WritableNativeMap result = new WritableNativeMap();
+        result.putString("data", "FILE_NOT_FOUND");
+        this.sendEvent(reactContext, "onException", result);
         return;
       }
 
       int width = bitmap.getWidth();
       int height = bitmap.getHeight();
-
       boolean hasAlpha = bitmap.hasAlpha();
 
+      WritableNativeMap result = new WritableNativeMap();
+      result.putInt("width", width);
+      result.putInt("height", height);
+      result.putBoolean("hasAlpha", hasAlpha);
+
+      this.sendEvent(reactContext, "onReadHeader", result);
+
       for (int x = 0; x < width; x++) {
+        WritableNativeMap dataResult = new WritableNativeMap();
+        WritableNativeArray pixels = new WritableNativeArray();
+
         for (int y = 0; y < height; y++) {
           int color = bitmap.getPixel(x, y);
           String hex = Integer.toHexString(color);
           pixels.pushString(hex);
         }
+
+        dataResult.putArray("pixels", pixels);
+        this.sendEvent(reactContext, "onReadData", dataResult);
       }
-
-      result.putInt("width", width);
-      result.putInt("height", height);
-      result.putBoolean("hasAlpha", hasAlpha);
-      result.putArray("pixels", pixels);
-
-      promise.resolve(result);
-
     } catch (Exception e) {
-      promise.reject(e);
+      WritableNativeMap result = new WritableNativeMap();
+      result.putString("data", e.getMessage());
+      this.sendEvent(reactContext, "onException", result);
     }
   }
 
   @ReactMethod
-  public void getSimpleGrayscalePixels(String filePath, Integer maxWidth, Integer maxHeight,  final Promise promise) {
+  public void getSimpleGrayscalePixels(String filePath, Integer maxWidth, Integer maxHeight, final Promise promise) {
     try {
       final int threshold = 127;
 
@@ -129,4 +136,11 @@ public class RNReactNativeImageDataModule extends ReactContextBaseJavaModule {
     return resizedBitmap;
   }
 
+  private void sendEvent(ReactContext reactContext,
+                       String eventName,
+                       @Nullable WritableMap params) {
+    reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit(eventName, params);
+  }
 }
