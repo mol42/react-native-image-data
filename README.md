d3adbc5bd86507ff47b015a67c41523e29be8e34
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2018-01-29 03:45:04 +0300

    ios image reading functionality integrated

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 
-# react-native-react-native-image-data
+# react-native-image-data
 
 ## Getting started
 
-`$ npm install react-native-react-native-image-data --save`
+`$ npm install react-native-image-data --save`
 
 ### Mostly automatic installation
 
-`$ react-native link react-native-react-native-image-data`
+`$ react-native link react-native-image-data`
 
 ### Manual installation
 
@@ -15,7 +15,7 @@
 #### iOS
 
 1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
-2. Go to `node_modules` ➜ `react-native-react-native-image-data` and add `RNReactNativeImageData.xcodeproj`
+2. Go to `node_modules` ➜ `react-native-image-data` and add `RNReactNativeImageData.xcodeproj`
 3. In XCode, in the project navigator, select your project. Add `libRNReactNativeImageData.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
 4. Run your project (`Cmd+R`)<
 
@@ -26,26 +26,17 @@
   - Add `new RNReactNativeImageDataPackage()` to the list returned by the `getPackages()` method
 2. Append the following lines to `android/settings.gradle`:
   	```
-  	include ':react-native-react-native-image-data'
-  	project(':react-native-react-native-image-data').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-react-native-image-data/android')
+  	include ':react-native-image-data'
+  	project(':react-native-image-data').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-image-data/android')
   	```
 3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
   	```
-      compile project(':react-native-react-native-image-data')
+      compile project(':react-native-image-data')
   	```
-
-#### Windows
-[Read it! :D](https://github.com/ReactWindows/react-native)
-
-1. In Visual Studio add the `RNReactNativeImageData.sln` in `node_modules/react-native-react-native-image-data/windows/RNReactNativeImageData.sln` folder to their solution, reference from their app.
-2. Open up your `MainPage.cs` app
-  - Add `using React.Native.Image.Data.RNReactNativeImageData;` to the usings at the top of the file
-  - Add `new RNReactNativeImageDataPackage()` to the `List<IReactPackage>` returned by the `Packages` method
-
 
 ## Usage
 ```javascript
-import RNReactNativeImageData from 'react-native-react-native-image-data';
+import RNReactNativeImageData from 'react-native-image-data';
 
 // TODO: What to do with the module?
 RNReactNativeImageData;
--- ios/RNReactNativeImageData.m
+++ ios/RNReactNativeImageData.m
@@ -1,13 +1,101 @@
 
 #import "RNReactNativeImageData.h"
+#import "UIImage+ColorAtPixel.h"
+#import <React/RCTImageLoader.h>
 
 @implementation RNReactNativeImageData
+
+@synthesize bridge = _bridge;
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(getSimpleGrayscalePixels:(NSString *)path
+                    options:(NSDictionary *)options
+                    findEventsWithResolver:(RCTPromiseResolveBlock)resolve
+                    rejecter:(RCTPromiseRejectBlock)reject)
+{
+
+    [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:path] callback:^(NSError *error, UIImage *image) {
+        if (error || image == nil) { // if couldn't load from bridge create a new UIImage
+            NSURL *imageUrl = [[NSURL alloc] initWithString:path];
+            image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
+
+            if (image == nil) {
+                NSError *error = @"image could not read";
+                reject(@"no_events", @"There were no events", error);
+                return;
+            }
+        }
+ 
+        NSInteger maxWidth = [RCTConvert NSInteger:options[@"maxWidth"]];
+        NSInteger maxHeight = [RCTConvert NSInteger:options[@"maxHeight"]];
+        CGSize newSize = CGSizeMake(maxWidth, maxHeight);
+        UIImage *scaledImage = [self scaleImage:image toSize:newSize];
+
+        /*
+        for (int i = 0; i < maxWidth; i++) {
+            for (int j = 0; j < maxHeight; j++) {
+
+            }
+        }
+        */
+        /*
+        if (options[@"width"] && options[@"height"]) {
+            NSInteger scaledWidth = [RCTConvert NSInteger:options[@"width"]];
+            NSInteger scaledHeight = [RCTConvert NSInteger:options[@"height"]];
+            float originalWidth = image.size.width;
+            float originalHeight = image.size.height;
+            
+            x = x * (originalWidth / scaledWidth);
+            y = y * (originalHeight / scaledHeight);
+            
+        }
+        */
+        //CGPoint point = CGPointMake(x, y);
+        
+        //UIColor *pixelColor = [image colorAtPixel:point];
+        //callback(@[[NSNull null], hexStringForColor(pixelColor)]);
+        resolve(null);
+    }];
+}
+
+- NSString * hexStringForColor( UIColor* color ) {
+    const CGFloat *components = CGColorGetComponents(color.CGColor);
+    CGFloat r = components[0];
+    CGFloat g = components[1];
+    CGFloat b = components[2];
+    NSString *hexString=[NSString stringWithFormat:@"#%02X%02X%02X", (int)(r * 255), (int)(g * 255), (int)(b * 255)];
+
+    return hexString;
+}
+
+- (UIImage *)scaleImage:(UIImage *)originalImage toSize:(CGSize)size
+{       
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(NULL, size.width, size.height, 8, 0, colorSpace, kCGImageAlphaPremultipliedLast);
+    CGContextClearRect(context, CGRectMake(0, 0, size.width, size.height));
+    
+    if (originalImage.imageOrientation == UIImageOrientationRight) {
+        CGContextRotateCTM(context, -M_PI_2);
+        CGContextTranslateCTM(context, -size.height, 0.0f);
+        CGContextDrawImage(context, CGRectMake(0, 0, size.height, size.width), originalImage.CGImage);
+    } else {
+        CGContextDrawImage(context, CGRectMake(0, 0, size.width, size.height), originalImage.CGImage);
+    }
+        
+    CGImageRef scaledImage = CGBitmapContextCreateImage(context);
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    
+    UIImage *image = [UIImage imageWithCGImage:scaledImage];
+    CGImageRelease(scaledImage);
+    
+    return image;
+}
 
 @end
   
