17c75fc2f19f78859c829083fd419c76595e94b4
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2018-07-26 17:43:25 -0400

    converting pixels array to image XY matrix integrated

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ const RNReactNativeImageDataWrapper = {
         } else {
             return RNReactNativeImageData.getSimpleGrayscalePixels(imagePath, scaledWidth, scaledHeight);
         }
+    },
+
+    getSimpleGrayscalePixelsAsXYMatrix : (imagePath, {scaledWidth, scaledHeight}) => {
+        
+        let imagePixelsArray = RNReactNativeImageDataWrapper.getSimpleGrayscalePixels(imagePath, {scaledWidth, scaledHeight});
+        let imageXYMatrix = [];
+
+        for (let i = 0; i < scaledWidth; i++) {
+            imageXYMatrix[i] = [];
+        }
+
+        let index = 0;
+
+        for (let i = 0; i < scaledWidth; i++) {
+            for (let j = 0; j < scaledHeight; j++) {
+                imageXYMatrix[j][i] = imagePixelsArray[index++];
+            }
+        }
+
+        return imageXYMatrix;
     }
 }
 
